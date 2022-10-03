a57a0a3468d4a9e1f9b09992f6f0d13609292da9
Dendi <rixsitillaevt@gmail.com>
2022-10-03 23:10:41 +0500

    feat: create Landing page

---
+++ pages/main/navigation.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector(".button__nav-menu");
+const navigation = document.querySelector(".nav__small");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+    navigation.classList.remove("display-none");
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    navigation.classList.add("display-none");
+  }
+});
