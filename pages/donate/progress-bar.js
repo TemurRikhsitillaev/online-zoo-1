2b21d6e3a740083a4ce646f0699084f21f51f3ec
Dendi <rixsitillaevt@gmail.com>
2022-10-06 01:43:05 +0500

    docs: add 640px resolution to donate page

--- pages/donate/progress-bar.js
+++ pages/donate/progress-bar.js
@@ -1,13 +1,20 @@
 const step = document.querySelector(".progress-circle");
 const circle = document.querySelector(".progress-circle");
 const allCircle = document.querySelectorAll(".progress-circle");
-const count = document.querySelector(".progress-count");
+const count = document.querySelector(".progress-count ");
 const countAll = document.querySelectorAll(".progress-count");
+const bodyWidth = document.querySelector("body").clientWidth;
+const dollar100 = document.querySelector(".dollar100");
+const dollar100text = document.querySelector(".dollar100text");
 
-allCircle.addEventListener("click", () => {
-  countAll.classList.remove("countActive");
-  count.classList.add("countActive");
+if (bodyWidth == 640) {
+  dollar100text.classList.add("countActive");
+  dollar100.classList.add("circleActive");
+}
+// allCircle.addEventListener("click", () => {
+//   countAll.classList.remove("countActive");
+//   count.classList.add("countActive");
 
-  allCircle.classList.remove("circleActive");
-  circle.classList.add("circleActive");
-});
+//   allCircle.classList.remove("circleActive");
+//   circle.classList.add("circleActive");
+// });
