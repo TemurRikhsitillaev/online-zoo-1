63d3f0d71fc3a4232e168552e5e0c9aa5282302f
Dendi <rixsitillaevt@gmail.com>
2022-10-10 20:05:50 +0500

    docs: add alert

--- pages/main/navigation.js
+++ pages/main/navigation.js
@@ -2,6 +2,10 @@ const menuBtn = document.querySelector(".button__nav-menu");
 const navigation = document.querySelector(".nav__small");
 let menuOpen = false;
 
+alert(
+  "Здравствуйте, сайт ещё в процессе разработки. Был бы очень рад если бы вы проверили мою работу 13 октября"
+);
+
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
