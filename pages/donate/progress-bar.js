b3827724faf1a177dd02dafa2be6bb7a72176cd9
Dendi <rixsitillaevt@gmail.com>
2022-10-13 14:39:23 +0500

    docs: add amount input

--- pages/donate/progress-bar.js
+++ pages/donate/progress-bar.js
@@ -7,14 +7,69 @@ const bodyWidth = document.querySelector("body").clientWidth;
 const dollar100 = document.querySelector(".dollar100");
 const dollar100text = document.querySelector(".dollar100text");
 
+const inputNumber = document.querySelectorAll(".input-amount");
+const inputNumberOne = document.querySelector(".input-amount");
+
+const inputNumberID = document.querySelector("#input__number");
+console.log(inputNumber.value);
+
 if (bodyWidth == 640 || bodyWidth == 320) {
   dollar100text.classList.add("countActive");
   dollar100.classList.add("circleActive");
+  inputNumberOne.value = `${100}`;
+} else if (bodyWidth > 640 || bodyWidth <= 1000) {
+  inputNumberOne.value = `${1000}`;
+} else if (bodyWidth > 320 || bodyWidth <= 640) {
+  dollar100text.classList.add("countActive");
+  dollar100.classList.add("circleActive");
+  inputNumberOne.value = `${100}`;
 }
-// allCircle.addEventListener("click", () => {
-//   countAll.classList.remove("countActive");
-//   count.classList.add("countActive");
+console.log(countAll);
 
-//   allCircle.classList.remove("circleActive");
-//   circle.classList.add("circleActive");
-// });
+allCircle.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    for (let i = 0; i < allCircle.length; i++) {
+      allCircle[i].classList.remove("circleActive");
+      countAll[i].classList.remove("countActive");
+    }
+    const currentDollar = e.currentTarget.getAttribute("data-dollar");
+    console.log(currentDollar);
+
+    e.currentTarget.classList.add("circleActive");
+    inputNumberOne.value = `${currentDollar}`;
+
+    for (let i = 0; i < countAll.length; i++) {
+      if (countAll[i].getAttribute("data-dollar") == currentDollar) {
+        inputNumber.placeholder = `$ ${currentDollar}`;
+        countAll[i].classList.add("countActive");
+      }
+    }
+  });
+});
+
+Array.from(inputNumber).forEach((input) => {
+  const min = +input.min;
+  const max = +input.max;
+
+  input.addEventListener("input", (e) => {
+    const value = +input.value;
+    if (value > max) {
+      input.value = max;
+    } else if (value < min) {
+      input.value = min;
+    }
+  });
+});
+
+console.log(inputNumberID.value);
+inputNumberID.oninput = () => {
+  console.log(inputNumberID.value);
+  for (let i = 0; i < countAll.length; i++) {
+    if (inputNumberID.value == countAll[i].getAttribute("data-dollar")) {
+      countAll.forEach((el) => el.classList.remove("countActive"));
+      allCircle.forEach((el) => el.classList.remove("circleActive"));
+      countAll[i].classList.add("countActive");
+      allCircle[i].classList.add("circleActive");
+    }
+  }
+};
